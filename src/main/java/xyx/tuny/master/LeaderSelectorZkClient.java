6b100b7bbfb9f61bb22f316ed39ede39e68cf060
trustnature <trustnature@outlook.com>
2018-08-22 23:07:34 +0800

    'master'

---
+++ src/main/java/xyx/tuny/master/LeaderSelectorZkClient.java
@@ -0,0 +1,68 @@
+package xyx.tuny.master;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * master选举
+ */
+public class LeaderSelectorZkClient {
+
+    //启动的服务个数
+    private static final int  CLIENT_QTY = 10;
+
+
+    public static void main(String[] args) throws Exception{
+
+        List<CuratorFramework> clients = new ArrayList<CuratorFramework>();
+        List<WorkServer>  workServers = new ArrayList<WorkServer>();
+
+        try{
+            for ( int i = 0; i < CLIENT_QTY; ++i ){
+                //创建zkClient
+                CuratorFramework client = CuratorFrameworkFactory.builder()
+                        .connectString("127.0.0.1:2181")
+                        .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                        .sessionTimeoutMs(5000)
+                        .build();
+                clients.add(client);
+                //创建serverData
+                RunningData runningData = new RunningData();
+                runningData.setCid(Long.valueOf(i));
+                runningData.setName("Client #" + i);
+                //创建服务
+                WorkServer workServer = new WorkServer(runningData);
+                workServer.setClient(client);
+
+                workServers.add(workServer);
+                workServer.start();
+            }
+
+            System.out.println("敲回车键退出！\n");
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+        }finally{
+            System.out.println("Shutting down...");
+
+            for ( WorkServer workServer : workServers ){
+                try {
+                    workServer.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            for ( CuratorFramework client : clients ){
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/xyx/tuny/master/WorkServer.java
@@ -0,0 +1,163 @@
+package xyx.tuny.master;
+
+import com.alibaba.fastjson.JSON;
+import org.I0Itec.zkclient.exception.ZkInterruptedException;
+import org.I0Itec.zkclient.exception.ZkNoNodeException;
+import org.I0Itec.zkclient.exception.ZkNodeExistsException;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.zookeeper.CreateMode;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class WorkServer {
+
+    //客户端状态
+    private volatile boolean running = false;
+
+    private CuratorFramework client;
+
+    //主节点路径
+    public static final String MASTER_PATH = "/master";
+
+    //监听(用于监听主节点删除事件)
+    private NodeCacheListener dataListener;
+
+    //服务器基本信息
+    private RunningData serverData;
+    //主节点基本信息
+    private RunningData masterData;
+    private NodeCache cache;
+    //调度器
+    private ScheduledExecutorService delayExector = Executors.newScheduledThreadPool(1);
+    //延迟时间5s
+    private int delayTime = 5;
+
+    public WorkServer(RunningData runningData){
+        this.serverData = runningData;
+    }
+
+    //启动
+    public void start() throws Exception{
+        if(running){
+            throw new Exception("server has startup....");
+        }
+        running = true;
+        client.start();
+        this.cache = new NodeCache(client,MASTER_PATH,false);
+
+        this.dataListener = new NodeCacheListener() {
+            @Override
+            public void nodeChanged() throws Exception {
+                System.out.println("masterData:" + masterData.getName());
+                if(masterData != null && masterData.getName().equals(serverData.getName())){//若之前master为本机,则立即抢主,否则延迟5秒抢主(防止小故障引起的抢主可能导致的网络数据风暴)
+                    System.out.println("previous masterData is same as " + serverData.getName());
+                    takeMaster();
+                }else{
+                    delayExector.schedule(new Runnable() {
+                        @Override
+                        public void run() {
+                            takeMaster();
+                        }
+                    },delayTime, TimeUnit.SECONDS);
+                }
+            }
+        };
+        this.cache.getListenable().addListener(dataListener);
+        try {
+            cache.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        takeMaster();
+    }
+
+    //停止
+    public void stop() throws Exception{
+        if(!running){
+            throw new Exception("server has stopped.....");
+        }
+        running = false;
+        delayExector.shutdown();
+        cache.getListenable().removeListener(dataListener);
+        releaseMaster();
+    }
+
+    //抢注主节点
+    private void takeMaster(){
+        if(!running) return ;
+
+        try {
+            client.create().withMode(CreateMode.PERSISTENT).forPath(MASTER_PATH, JSON.toJSONString(serverData).getBytes());
+            masterData = serverData;
+            System.out.println(serverData.getName()+" is master");
+
+            delayExector.schedule(new Runnable() {//测试抢主用,每5s释放一次主节点
+                @Override
+                public void run() {
+                    if(checkMaster()){
+                        releaseMaster();
+                    }
+                }
+            },5,TimeUnit.SECONDS);
+        }catch (ZkNodeExistsException e){//节点已存在
+            System.out.println("node exists");
+            String retJson = new String(cache.getCurrentData().getData());
+            RunningData runningData = null;
+            try {
+                runningData = (RunningData)JSON.parseObject(retJson, RunningData.class);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            if(runningData == null){//读取主节点时,主节点被释放
+                takeMaster();
+            }else{
+                masterData = runningData;
+            }
+        } catch (Exception e) {
+            // ignore;
+        }
+
+    }
+    //释放主节点
+    private void releaseMaster(){
+        if(checkMaster()){
+            try {
+                client.delete().forPath(MASTER_PATH);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //检验自己是否是主节点
+    private boolean checkMaster(){
+        try {
+            String retJson = new String(cache.getCurrentData().getData());
+            RunningData runningData = (RunningData)JSON.parseObject(retJson, RunningData.class);
+            masterData = runningData;
+            if (masterData.getName().equals(serverData.getName())) {
+                return true;
+            }
+            return false;
+
+        }catch (ZkNoNodeException e){//节点不存在
+            return  false;
+        }catch (ZkInterruptedException e){//网络中断
+            return checkMaster();
+        }catch (Exception e){//其它
+            return false;
+        }
+    }
+
+    public void setClient(CuratorFramework client) {
+        this.client = client;
+    }
+
+    public CuratorFramework getClient() {
+        return client;
+    }
+}
